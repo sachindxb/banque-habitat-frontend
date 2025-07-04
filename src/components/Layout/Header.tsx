f5d26ea33dcad1d19243e0b2461392d814b910f1
sachindxb <sachinshukladxb@gmail.com>
2025-07-04 17:19:03 +0400

    fix: header working, alligned

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,9 +1,8 @@
 // Header.tsx
-import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css'; // We'll create this next
 
-const Header: FC = () => {
+const Header = () => {
   return (
     <header className="header">
       <div className="header-container">
@@ -17,6 +16,12 @@ const Header: FC = () => {
           <div className="nav-item services-dropdown">
             <span>Services</span>
             <div className="dropdown-icon" />
+            <div className="dropdown-menu">
+              <Link to="/purchase" className="dropdown-item">PURCHASE</Link>
+              <Link to="/renovation-usd" className="dropdown-item">RENOVATION USD</Link>
+              <Link to="/construction" className="dropdown-item">Construction</Link>
+              <Link to="/renovation-lbp" className="dropdown-item">RENOVATION LBP</Link>
+            </div>
           </div>
 
           <Link to="/housing-loans" className="nav-item">Housing Loans</Link>
@@ -26,9 +31,13 @@ const Header: FC = () => {
         <div className="right-section">
           <Link to="/contact" className="nav-item">Contact</Link>
           <div className="language-selector">
+            <span className="language-emoji"></span>
             <span>EN</span>
-            <span className="language-divider">|</span>
-            <span>AR</span>
+            <div className="dropdown-icon" />
+            <div className="dropdown-menu">
+              <span className="dropdown-item">English</span>
+              <span className="dropdown-item">العربية</span>
+            </div>
           </div>
           <button className="signup-button">Sign Up</button>
         </div>
